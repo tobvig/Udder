372855dd7e1d78cc95734194c4b17f8e4fa06974
Tobias <tobias.vig@gmail.com>
2020-05-19 11:32:43 +0200

    Add labels

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tobias Vig on 19/05/2020.
+//  Copyright © 2020 Tobias Vig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
